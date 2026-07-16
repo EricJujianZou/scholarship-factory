590562bdae9f193648b27c40aaa11d5978451da9
EricJujianZou <e2zou@uwaterloo.ca>
2026-07-16 08:25:03 -0400

    adw auto: edit scholarship_factory/__init__.py

--- scholarship_factory/__init__.py
+++ scholarship_factory/__init__.py
@@ -5,6 +5,7 @@ from .jsonld import extract_jsonld
 from .models import Opportunity, Provenance
 from .parse_dates import parse_deadline_dates, typed_deadlines
 from .parse_money import MoneyBound, MoneyValue, parse_money, typed_cost, typed_reward
+from .polite import DEFAULT_MIN_INTERVAL, PoliteFetcher
 from .profile import ApplicantProfile, ProfileStore
 from .seeds import Seed, SeedType, load_seeds
 from .store import OpportunityStore
